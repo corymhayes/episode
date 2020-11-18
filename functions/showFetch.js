50e8c01863bfeba71bf8d58dfae0ae61e900da37
Cory Hayes <corymhayes@gmail.com>
2020-11-18 15:43:27 -0600

    fix lambda

--- functions/showFetch.js
+++ functions/showFetch.js
@@ -1,7 +1,7 @@
-const fetch = require('node-fetch')
+// const fetch = require('node-fetch')
 
 exports.handler = async function(event, context) {
-  const query = event.queryStringParameters.show
+  // const query = event.queryStringParameters.show
 
   // const showSearch = await fetch(`https://api.themoviedb.org/3/search/tv?api_key=a3014d2e6db95934fd2619ce49e4cbad&language=en-US&page=1&query=${query}&include_adult=false`)
   // const showData = await showSearch.json()
