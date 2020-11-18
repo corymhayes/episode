5b4c0f4280187487e959ff39153cf03f4707b6a8
Cory Hayes <corymhayes@gmail.com>
2020-11-18 15:07:59 -0600

    wip lambda

--- functions/showFetch.js
+++ functions/showFetch.js
@@ -5,29 +5,34 @@ exports.handler = async function(event, context) {
 
   const showSearch = await fetch(`https://api.themoviedb.org/3/search/tv?api_key=a3014d2e6db95934fd2619ce49e4cbad&language=en-US&page=1&query=${query}&include_adult=false`)
   const showData = await showSearch.json()
-  
-  if (showData.results) {
-    const showId = showData.results[0].id
-    const numberOfSeasonsSearch = await fetch(`https://api.themoviedb.org/3/tv/${showId}?api_key=a3014d2e6db95934fd2619ce49e4cbad&language=en-US`)
-    const numberOfSeasonsData = await numberOfSeasonsSearch.json()
-    const randomSeasonNumber = getRandomNumber(numberOfSeasonsData.number_of_seasons)
-  
-    const seasonSearch = await fetch(`https://api.themoviedb.org/3/tv/${showId}/season/${randomSeasonNumber}?api_key=a3014d2e6db95934fd2619ce49e4cbad&language=en-US`)
-    const numberOfEpisodesData = await seasonSearch.json()
 
-    const randomEpisode = numberOfEpisodesData.episodes[getRandomNumber(numberOfEpisodesData.episodes.length)]
-   
-    return {
-      statusCode: 200,
-      body: JSON.stringify({
-        show_name: query,
-        episode_number: randomEpisode.episode_number,
-        name: randomEpisode.name,
-        overview: randomEpisode.overview,
-        season_number: randomEpisode.season_number,
-        vote_average: randomEpisode.vote_average,
-        poster_path: numberOfEpisodesData.poster_path
-      })
-    }
+  return { 
+    statusCode: 200,
+    body: JSON.stringify({ showData })
   }
+  
+  // if (showData.results) {
+  //   const showId = showData.results[0].id
+  //   const numberOfSeasonsSearch = await fetch(`https://api.themoviedb.org/3/tv/${showId}?api_key=a3014d2e6db95934fd2619ce49e4cbad&language=en-US`)
+  //   const numberOfSeasonsData = await numberOfSeasonsSearch.json()
+  //   const randomSeasonNumber = getRandomNumber(numberOfSeasonsData.number_of_seasons)
+  
+  //   const seasonSearch = await fetch(`https://api.themoviedb.org/3/tv/${showId}/season/${randomSeasonNumber}?api_key=a3014d2e6db95934fd2619ce49e4cbad&language=en-US`)
+  //   const numberOfEpisodesData = await seasonSearch.json()
+
+  //   const randomEpisode = numberOfEpisodesData.episodes[getRandomNumber(numberOfEpisodesData.episodes.length)]
+   
+  //   return {
+  //     statusCode: 200,
+  //     body: JSON.stringify({
+  //       show_name: query,
+  //       episode_number: randomEpisode.episode_number,
+  //       name: randomEpisode.name,
+  //       overview: randomEpisode.overview,
+  //       season_number: randomEpisode.season_number,
+  //       vote_average: randomEpisode.vote_average,
+  //       poster_path: numberOfEpisodesData.poster_path
+  //     })
+  //   }
+  // }
 }
