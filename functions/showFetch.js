c8018a697831565893c56e5f81b7904ad7b23c6f
Cory Hayes <cor33e@Corys-MBP.austin.rr.com>
2023-02-13 08:40:02 -0600

    fixed fetch

--- functions/showFetch.js
+++ functions/showFetch.js
@@ -8,15 +8,14 @@ exports.handler = async function(event, context) {
   const query = event.queryStringParameters.show
   const showSearch = await fetch(`https://api.themoviedb.org/3/search/tv?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=1&query=${query}&include_adult=false`)
   const showData = await showSearch.json()
-  console.log(showData)
   
   if (showData.results) {
     const showId = showData.results[0].id
-    const numberOfSeasonsSearch = await fetch(`https://api.themoviedb.org/3/tv/${showId}?api_key=a3014d2e6db95934fd2619ce49e4cbad&language=en-US`)
+    const numberOfSeasonsSearch = await fetch(`https://api.themoviedb.org/3/tv/${showId}?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`)
     const numberOfSeasonsData = await numberOfSeasonsSearch.json()
     const randomSeasonNumber = getRandomNumber(numberOfSeasonsData.number_of_seasons)
   
-    const seasonSearch = await fetch(`https://api.themoviedb.org/3/tv/${showId}/season/${randomSeasonNumber}?api_key=a3014d2e6db95934fd2619ce49e4cbad&language=en-US`)
+    const seasonSearch = await fetch(`https://api.themoviedb.org/3/tv/${showId}/season/${randomSeasonNumber}?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`)
     const numberOfEpisodesData = await seasonSearch.json()
 
     const randomEpisode = numberOfEpisodesData.episodes[getRandomNumber(numberOfEpisodesData.episodes.length)]
